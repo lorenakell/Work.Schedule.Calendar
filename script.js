a9c5258922284163931cf4bab8f5111bdd635151
Kell(er) Instincts <68985927+lorenakell@users.noreply.github.com>
2020-09-02 04:51:57 -0500

    added function to adjust style based on current time

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var hourlyArray;
 var currentTime = moment();
+var textBlock = $(".col-8");
 
 if (localStorage.getItem("localHourlyTasks")) {
     hourlyArray = JSON.parse(localStorage.getItem("localHourlyTasks"));
@@ -8,3 +9,16 @@ if (localStorage.getItem("localHourlyTasks")) {
 }
 
 $("#currentDay").text(`${currentTime.format('dddd, MMMM Do')}`);
+
+function updateCurrentScheduleTime( ) {
+    textBlock.removeClass('past present future');
+    $.each(textBlock, function(scheduleBlockHour) {
+        if)scheduleBlockHour < (currentTime.hour()-9)) {
+            $(this).addClass('past');
+        } else if (scheduleBlockHour == (currentTime.hour()-9)) {
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('future');
+        }
+    })
+}
