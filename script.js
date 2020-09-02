0ad6734d62f847d884abfa4fd9b4ba8294b691db
Kell(er) Instincts <68985927+lorenakell@users.noreply.github.com>
2020-09-02 04:40:20 -0500

    displaying current date using Moment.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var hourlyArray;
+var currentTime = moment();
 
 if (localStorage.getItem("localHourlyTasks")) {
     hourlyArray = JSON.parse(localStorage.getItem("localHourlyTasks"));
 } else {
     hourlyArray =[];
 }
+
+$("#currentDay").text(`${currentTime.format('dddd, MMMM Do')}`);
